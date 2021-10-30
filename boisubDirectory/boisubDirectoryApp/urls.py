ef13e8060c46584fe5091acce510af38e6b8a898
Ashli Rankin <42153710+Ashlirankin18@users.noreply.github.com>
2021-10-30 17:00:30 -0400

    updated files

--- boisubDirectory/boisubDirectoryApp/urls.py
+++ boisubDirectory/boisubDirectoryApp/urls.py
@@ -7,7 +7,20 @@ urlpatterns = [
     url(r'^influencers/$', views.Influencers.as_view(), name='influencers'),
     url(r'^$', views.BooksView.as_view(), name='books'),
     url(r'^$', views.AuthorsView.as_view(), name='authors'),
+    url(r'^$', views.BeautyView.as_view(), name='beauty'),
+    url(r'^$', views.FashionView.as_view(), name='fashion'),
+    url(r'^$', views.HealthView.as_view(), name='health'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^$', views.ServiceView.as_view(), name='services'),
+    url(r'^$', views.FoodView.as_view(), name='food'),
+    url(r'^business$', views.BusinessView.as_view(), name='business'),
     path('featuredpersons/', views.influencers),
     path('books/', views.books),
-    path('authors/', views.authors)
-]
+    path('authors/', views.authors),
+    path('businesses/beauty', views.beauty),
+    path('businesses/fashion', views.fashion),
+    path('businesses/health', views.health),
+    path('businesses/home', views.homeDecor),
+    path('businesses/services', views.services),
+    path('businesses/food', views.food)
+    ]
