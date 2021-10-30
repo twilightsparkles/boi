ee46576dbeebe1b795ac7a4af0cf4d50b560c9cb
Ashli Rankin <42153710+Ashlirankin18@users.noreply.github.com>
2021-10-30 01:59:46 -0400

    Updated authors

--- boisubDirectory/boisubDirectoryApp/urls.py
+++ boisubDirectory/boisubDirectoryApp/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'),
     url(r'^influencers/$', views.Influencers.as_view(), name='influencers'),
-    path('featuredpersons/', views.influencers)
+    url(r'^$', views.BooksView.as_view(), name='books'),
+    url(r'^$', views.AuthorsView.as_view(), name='authors'),
+    path('featuredpersons/', views.influencers),
+    path('books/', views.books),
+    path('authors/', views.authors)
 ]
--- boisubDirectory/boisubDirectoryApp/views.py
+++ boisubDirectory/boisubDirectoryApp/views.py
@@ -22,9 +22,6 @@ import requests
 #     context = {'books': book_data}
 #     return render(request, "index.html", context)
 
-# def home_page(request):
-
-#     return render(request, "BlackDatabase.html")
 
 
 def influencers(request):
@@ -42,9 +39,49 @@ def influencers(request):
     context = {'influencers': influencer_data}
     return render(request, "Influencers.html", context)
 
+def books(request):
+    url = 'https://blackindex.herokuapp.com/books'
+    book_data = []
+
+    books = requests.get(url).json()
+
+    for book in books:
+        book_context = {
+            "image_url": book["image_url"],
+            "author_name" : book["author_name"],
+            "title": book["title"],
+            "book_description": book["description"]
+        }
+        book_data.append(book_context)
+
+    context = {'books': book_data}
+    return render(request, "books.html", context)
+
+def authors(request):
+    url = 'https://blackindex.herokuapp.com/authors'
+    authors_data = []
+
+    authors = requests.get(url).json()
+
+    for author in authors:
+        author_context = {
+            "name" : author["author_first_name"] + author["author_last_name"],
+            "image_url": author["image_url"],
+            "bio": author["biography"]
+        }
+        authors_data.append(author_context)
+
+    context = {'authors': authors_data}
+    return render(request, "authors.html", context)
 
 class HomePageView(TemplateView):
     template_name = "BlackDatabase.html"
 
 class Influencers(TemplateView):
     template_name = "Influencers.html"
+
+class BooksView(TemplateView):
+    template_name = "books.html"
+
+class AuthorsView(TemplateView):
+    template_name = "authors.html"
